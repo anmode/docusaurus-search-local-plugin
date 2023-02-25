9435f7e7e36e07e71071d8de4554337fce1bde36
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-02-25 11:09:49 +0530

    updated with the fix

---
+++ lib/client/theme/SearchBar/HighlightSearchResults.d.ts
@@ -0,0 +1,7 @@
+export type DSLALocationState = {
+    cmfcmfhighlight?: {
+        terms: string[];
+        isDocsOrBlog: boolean;
+    };
+};
+export declare function HighlightSearchResults(): null;
